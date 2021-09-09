c77ebf83337042fcef8f8ea58a07fd9afa9c97e4
garrettbslone <gsloneca@gmail.com>
2021-09-09 12:49:34 -0700

    add starter for checking order

--- Assets/Scripts/Ingredients/IngredientSpawner.cs
+++ Assets/Scripts/Ingredients/IngredientSpawner.cs
@@ -10,10 +10,15 @@ public class IngredientSpawner : MonoBehaviour
     {
         SpawnCommand spawn = new SpawnCommand(this, ingredient);
         spawn.Execute();
+
+        CheckOrderManager.Instance.AddIngredientEntered(ingredient.GetComponent<Ingredient>());
+        Debug.Log("Ingredient added");
     }
     
     public void DespawnIngredient()
     {
         CommandManager.Instance.Undo();
+        
+        CheckOrderManager.Instance.RemoveIngredientEntered(gameObject.GetComponent<Ingredient>());
     }
 }
